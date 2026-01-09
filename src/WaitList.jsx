aa0363be1304484055bac4777fd489fc2f87266c
ololade <belloololade2003@gmail.com>
2026-01-09 14:49:30 +0100

    new deployment

--- src/WaitList.jsx
+++ src/WaitList.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import logo from "./assets/logo.png";
 
 const WaitList = () => {
   return (
-    <div>WaitList</div>
-  )
-}
+    <div className="wait-wrap">
+      <div className="card-wrap">
+        <img src={logo} alt="" width={120} />
+        <form action="">
+          <div className="inputss">
+            <div className="input-wrap">
+              <label htmlFor="">First Name</label>
+              <input type="text" placeholder="Enter your first name" />
+            </div>
+            <div className="input-wrap">
+              <label htmlFor="">Last Name</label>
+              <input type="text" placeholder="Enter your last name" />
+            </div>
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="">Phone Number</label>
+            <input type="text" placeholder="Enter your phone number" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="">Email Address</label>
+            <input type="text" placeholder="Enter your email address" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default WaitList
+export default WaitList;
