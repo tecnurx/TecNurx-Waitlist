6c6779db0263358264d1d048798780e95bf72284
ololade <belloololade2003@gmail.com>
2026-01-09 15:36:29 +0100

    push

--- src/WaitList.jsx
+++ src/WaitList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./assets/logo.png";
+import line from "./assets/line.svg";
 
 const WaitList = () => {
   return (
@@ -7,25 +8,32 @@ const WaitList = () => {
       <div className="card-wrap">
         <img src={logo} alt="" width={120} />
         <form action="">
-          <div className="inputss">
-            <div className="input-wrap">
-              <label htmlFor="">First Name</label>
-              <input type="text" placeholder="Enter your first name" />
+          <div className="line-wrap">
+            <img src={line} alt="" className="line" />
+            <div className="forms">
+              <div className="inputss">
+                <div className="input-wrap">
+                  <label htmlFor="">First Name</label>
+                  <input type="text" placeholder="Enter your first name" />
+                </div>
+                <div className="input-wrap">
+                  <label htmlFor="">Last Name</label>
+                  <input type="text" placeholder="Enter your last name" />
+                </div>
+              </div>
+              <div className="input-wrap">
+                <label htmlFor="">Phone Number</label>
+                <input type="text" placeholder="Enter your phone number" />
+              </div>
+              <div className="input-wrap">
+                <label htmlFor="">Email Address</label>
+                <input type="text" placeholder="Enter your email address" />
+              </div>
             </div>
-            <div className="input-wrap">
-              <label htmlFor="">Last Name</label>
-              <input type="text" placeholder="Enter your last name" />
-            </div>
           </div>
-          <div className="input-wrap">
-            <label htmlFor="">Phone Number</label>
-            <input type="text" placeholder="Enter your phone number" />
-          </div>
-          <div className="input-wrap">
-            <label htmlFor="">Email Address</label>
-            <input type="text" placeholder="Enter your email address" />
-          </div>
-          <button type="submit">Submit</button>
+          <div className="btn">
+            <button type="submit">Submit</button>
+          </div>{" "}
         </form>
       </div>
     </div>
